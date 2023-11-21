e301915364ed9e96d7cc85217eecf12845be4af3
fureszb <fureszb@gmail.com>
2023-11-21 21:28:48 +0100

    PublicAndPrivate

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WriterController;
+use App\Http\Controllers\FavoritController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,12 @@ Route::get('/writers/{id}', [WriterController::class, 'show']);
 Route::get('/writers/{id}/edit', [WriterController::class, 'edit']);
 Route::put('/writers/{id}', [WriterController::class, 'update']);
 Route::delete('/writers/{id}', [WriterController::class, 'destroy']);
+
+
+
+Route::prefix('favorits')->group(function () {
+    Route::get('/', [FavoritController::class, 'index']);
+    Route::get('/{id}', [FavoritController::class, 'show']);
+    Route::post('/', [FavoritController::class, 'store']);
+    Route::delete('/{id}', [FavoritController::class, 'destroy']);
+});
